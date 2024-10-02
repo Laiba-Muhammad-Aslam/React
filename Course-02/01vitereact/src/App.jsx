06e3762e79f81be11cfcb9895a9ae10e24dfe3bd
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-10-02 12:33:47 +0500

    Create our own react library and JSX.

--- Course-02/01vitereact/src/App.jsx
+++ Course-02/01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 import Coffee from "./coffee"
 
 function App() {
+  const username = "Laiba Muhammad Aslam"
 
   return (
    <>
     <Coffee/>
-   <h1>Hello People</h1>
+   <h1>Hello People || This is {username}</h1>
    <p>We provide the best services</p>
    </>
   )
--- Course-02/01vitereact/src/main.jsx
+++ Course-02/01vitereact/src/main.jsx
@@ -1,7 +1,40 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+// import {jsx as _jsx} from "react/jsx-runtime.js"
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom App !</h1>
+        </div>
+    )
+}
+
+// const reactElement = {
+//     type: 'a',
+//     props: {
+//         href: "https://google.com",
+//         target: "_blank"
+//     },
+//     children: "Click me to visit google"
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser = "Laiba Khan"
+
+const reactElement = React.createElement(
+    'a',
+    {href: "https://google.com", target: "_blank"},
+    "Click me to visit your fav website",
+    anotherUser
+)
 createRoot(document.getElementById('root')).render(
-    <App />
+    // anotherElement
+    reactElement
+    // <App/>
 )
