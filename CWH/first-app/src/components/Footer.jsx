c3c75215a6ec4fac193761540444c9393bcf4f39
Laiba Muhammad Aslam <145155271+Laiba-Muhammad-Aslam@users.noreply.github.com>
2024-11-24 20:06:10 +0500

    first app in react

---
+++ CWH/first-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className='footer'>
+      Laiba Muhammad Aslam | All rights reserved
+    </div>
+  )
+}
+
+export default Footer
